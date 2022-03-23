6daa1a579e5f87617f9f001b54a42b93c45d339a
Mike <rans@un.org>
2022-03-23 16:46:58 +1300

    Update South Sudan Add Ukraine

---
+++ iati/ukraine_checks.py
@@ -0,0 +1,20 @@
+class UkraineChecks:
+    @staticmethod
+    def has_desired_scope(scopes):
+        return False
+
+    @staticmethod
+    def has_desired_marker(markers):
+        return False
+
+    @staticmethod
+    def has_desired_tag(tags):
+        return False
+
+    @staticmethod
+    def has_desired_sector(sectors):
+        return False
+
+    @staticmethod
+    def is_desired_narrative(narratives):
+        return False
