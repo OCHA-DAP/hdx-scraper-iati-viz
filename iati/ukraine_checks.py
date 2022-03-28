3eddaa550571aa9338f09ea2a1425f8700692449
Mike <rans@un.org>
2022-03-29 11:04:37 +1300

    Add Ukraine scope check

--- iati/ukraine_checks.py
+++ iati/ukraine_checks.py
@@ -1,6 +1,20 @@
 class UkraineChecks:
     @staticmethod
     def has_desired_scope(scopes):
+        """Check if the Ukraine code is present"""
+        for scope in scopes:
+            if (
+                scope.type == "1"
+                and scope.vocabulary == "1-2"
+                and scope.code.upper() == "OT-2022-000157-UKR"
+            ):
+                return True
+            elif (
+                scope.type == "2"
+                and scope.vocabulary == "2-1"
+                and scope.code.upper() in ("FUKR22", "HUKR22", "RYKRN22")
+            ):
+                return True
         return False
 
     @staticmethod
